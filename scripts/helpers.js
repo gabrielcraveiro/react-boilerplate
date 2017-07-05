86ddfdbf2d751d9a4144ee2469367ca062222f06
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-07-05 19:23:23 -0300

    Ep 10

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,11 +1,11 @@
-let helpers =  {
-  formatPrice :  function(cents) {
-    return '$' + ( (cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") );
+let helpers = {
+  formatPrice: function (cents) {
+    return '$' + ((cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
   },
-  rando : function(arr) {
+  rando: function (arr) {
     return arr[Math.floor(Math.random() * arr.length)];
   },
-  slugify : function(text) {
+  slugify: function (text) {
     return text.toString().toLowerCase()
       .replace(/\s+/g, '-')           // Replace spaces with -
       .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
@@ -13,11 +13,11 @@ let helpers =  {
       .replace(/^-+/, '')             // Trim - from start of text
       .replace(/-+$/, '');            // Trim - from end of text
   },
-  getFunName : function() {
+  getFunName: function () {
     var adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
-    
+
     var nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
-    
+
     return `${this.rando(adjectives)}-${this.rando(adjectives)}-${this.rando(nouns)}`;
   }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,95 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
+
+var h = require('./helpers');
+
+// Faz o browser precisar de browser state
+var createBrowserHistory = require('history/lib/createBrowserHistory')
+
+/* Router precisa de
+
+  Router
+  Route
+  Navigation
+
+*/
+
+/* 
+  App
+*/
+
+var App = React.createClass({
+
+  render: function () {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+
+})
+
+/*
+  Header
+  <Header />
+*/
+
+var Header = React.createClass({
+  render: function () {
+    return (
+      <header className="top">
+        <h1>
+          Catch
+        <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+})
+
+/*
+  Order
+  <Order />
+*/
+
+var Order = React.createClass({
+  render: function () {
+    return (
+      <p>Order</p>
+    )
+  }
+})
+
+/*
+  Inventory
+  <Inventory />
+*/
+
+var Inventory = React.createClass({
+  render: function () {
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
+
 /* 
   StorePicker
   um componente tem a variavel que cria uma nova classe, 
@@ -12,20 +101,46 @@ var ReactDOM = require('react-dom');
 
 var StorePicker = React.createClass({
 
+  mixins: [History],
+  goToStore: function (event) {
+    event.preventDefault();
+    // pegar os dados do input
+    var storeId = this.refs.storeId.value;
+    // mudar de storePicker para app
+    this.history.pushState(null, '/store/' + storeId);
+  },
+
   render: function () {
     // Variaveis são escritas normalmente, sendo colocadas entre {}, comentarios dentro de return são complicados e é melhor evitar
     // Ex: var name = "Gabriel"
     // <h2>Seu nome é {name}</h2>
-    return ( 
-      <form action="" className="store-selector">
+    return (
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" ref="storeID" />
-        <input type="submit"/>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="submit" />
       </form>
-    ) 
+    )
   }
 
 });
 
+var NotFound = React.createClass({
+  render: function () {
+    return <h1>Not found</h1>
+  }
+})
 
-ReactDOM.render(<StorePicker />, document.querySelector('#main')); 
+/*
+  Rotas
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path='*' component={NotFound} />
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
